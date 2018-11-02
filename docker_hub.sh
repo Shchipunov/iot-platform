1e4c55c8fc75815190b4e38e3dc87edcfca90453
svshc <svshc@DESKTOP-8SK7UDH>
2018-11-02 23:22:20 +0300

    Initial commit

--- docker_hub.sh
+++ docker_hub.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-echo "$TRAVIS_REPO_SLUG"
-echo "$TRAVIS_REPO_SLUG:$TAG"
-docker build -t $TRAVIS_REPO_SLUG:$TAG .
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
