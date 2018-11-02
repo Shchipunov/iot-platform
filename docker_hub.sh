5530d6b7937c14fb918b299073471251c5083ac3
svshc <svshc@DESKTOP-8SK7UDH>
2018-11-02 23:06:21 +0300

    Initial commit

--- docker_hub.sh
+++ docker_hub.sh
@@ -1,8 +1,11 @@
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
+#!/bin/bash
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+echo "$TRAVIS_REPO_SLUG"
+echo "$TRAVIS_REPO_SLUG:$TAG"
+docker build -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
