cd7edf1012066ed51685bbef600474c8b81afa07
svshc <svshc@DESKTOP-8SK7UDH>
2018-11-03 17:29:15 +0300

    Initial commit

--- heroku.sh
+++ heroku.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
-docker tag shchipunov/iot-platform registry.heroku.com/test-iot-platform
-docker push registry.heroku.com/test-iot-platform
+docker tag shchipunov/iot-platform registry.heroku.com/test-iot-platform/web
+docker push registry.heroku.com/test-iot-platform/web
+heroku container:release web --app $HEROKU_APPLICATION
