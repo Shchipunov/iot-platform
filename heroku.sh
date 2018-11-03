3c7923df529003070dd15b7b7c691ba81cfbecbf
svshc <svshc@DESKTOP-8SK7UDH>
2018-11-03 19:50:54 +0300

    Initial commit

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-docker tag shchipunov/iot-platform registry.heroku.com/test-iot-platform/web
-docker push registry.heroku.com/test-iot-platform/web
-heroku container:release web --app $HEROKU_APPLICATION
+docker_image = $(docker inspect registry.heroku.com/test-iot-platform/web --format={{.Id}})
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APPLICATION/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "'"$docker_image"'"
+    },
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
