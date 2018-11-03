215f0212f7a807f6a1dbeff753fd68c2010a4197
svshc <svshc@DESKTOP-8SK7UDH>
2018-11-03 21:14:31 +0300

    Initial commit

--- heroku.sh
+++ heroku.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-docker_image = $(docker inspect registry.heroku.com/test-iot-platform/web --format={{.Id}})
-curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APPLICATION/formation \
-  -d '{
-  "updates": [
-    {
-      "type": "web",
-      "docker_image": "'"$docker_image"'"
-    },
-  ]
-}' \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-container-registry
+docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APPLICATION
